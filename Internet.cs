8c0f65d9f33ff870283249dc8a3c0d67f49d5d2d
Amit Patange <amitpatange88@gmail.com>
2018-09-09 17:39:43 +0530

    added new class as utility for internet things

---
+++ Internet.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Org.Apollo.Utility
+{
+    class Internet
+    {
+        /// <summary>
+        /// This checks if internet connection is ACTIVE or not.
+        /// Method : Static
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsConnectionActive()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://example.com/"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
